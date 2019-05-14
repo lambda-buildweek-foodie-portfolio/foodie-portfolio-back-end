0452aefcb0353f452fe3490fccc040a3357bf999
TERR1E <terrieb.kim@gmail.com>
2019-05-13 23:01:12 -0700

    Set up server.js and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./server');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
