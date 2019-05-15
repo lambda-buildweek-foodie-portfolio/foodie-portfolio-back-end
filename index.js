9c966a9ce40a5b59fe80b41322757b46c4100193
TERR1E <terrieb.kim@gmail.com>
2019-05-14 18:14:39 -0700

    Add cors and helmet middleware

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./server');
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
 
 module.exports = server;
